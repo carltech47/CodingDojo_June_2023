087839d83d40f1234bac9e8cb309f9c857f16fa8
Toe Head <torreyfeacher@gmail.com>
2023-03-08 21:06:07 -0500

    CodingDojo_2023_2nd_Day_files

---
+++ Python/fundamentals/loop_over_list_elements.py
@@ -0,0 +1,3 @@
+my_list = ["truck", 2018, "blue"]
+for integer in range(0, len(my_list)):
+    print(integer, my_list[integer])
---
+++ Python/fundamentals/while_loop_counting_down.py
@@ -0,0 +1,6 @@
+y = 50
+while y > 0:
+    print("Counting down - ", y)
+    y = y - 1
+else:
+    print("Final else statement!!")
