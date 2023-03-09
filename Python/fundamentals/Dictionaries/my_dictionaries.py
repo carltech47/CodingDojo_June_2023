dc969dabfba9882760d150b18c233dc0251bae9e
Toe Head <torreyfeacher@gmail.com>
2023-03-09 18:30:09 -0500

    Dictionaries Test Data

---
+++ Python/fundamentals/Dictionaries/my_dictionaries.py
@@ -0,0 +1,17 @@
+# Challenge yourself!
+
+person = {"first_name": "Ada", "last_name": "Lovelace", "age": 42, "is_organ_donor": True}
+# Create a another person dictionary called person_2 and print it to the terminal
+person_2 = {"first_name": "Matilda", "last_name": "Smith", "age": 21, "is_organ_donor": True}
+
+capitals = {}
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
+# Add 2 key-value pairs to this dictionary.
+capitals["usa"] = "Washington, DC"
+capitals["fra"] = "Paris"
+
+# print the capitals dictionary to see how it changed!
+print([capitals])
+print(capitals["fra"])
