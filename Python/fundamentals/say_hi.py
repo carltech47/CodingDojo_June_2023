ad89a51a26610fd8a5afc62919ee65a686c6a27b
Toe Head <torreyfeacher@gmail.com>
2023-03-08 21:58:13 -0500

    Python Fundamentals file_say_hi_update

---
+++ Python/fundamentals/say_hi.py
@@ -0,0 +1,32 @@
+# Say Hi - Function
+
+def say_hi(name):
+    print("Hi, " + name)    
+    return name
+
+say_hi('Torrey')
+say_hi('Abrialla')
+say_hi('Melissa')
+say_hi('Brittany')
+
+
+
+
+"""
+# Challenge 1:
+#   Fill in the missing code for the full_name function.
+#   Be sure to add the 2 parameters (and name them appropriately)
+#   Return the full name to get the desired output!
+
+def full_name(first_name, last_name):
+    # your code here!
+    #print("Hello, " "+  first_name  + last_name)
+    print("Hello" , first_name + last_name)	#
+
+name1 = full_name("Eddie ", "Aikau")
+print(name1) # should print Eddie Aikau
+
+# Challenge 2: 
+#   Call the function again using your own name and print the results!
+full_name('Torrey ', 'Feacher') 
+"""
