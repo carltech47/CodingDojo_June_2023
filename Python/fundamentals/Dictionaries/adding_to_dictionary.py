a1354382a984f4ac2e4f8da334c448faf92179a4
Toe Head <torreyfeacher@gmail.com>
2023-03-09 18:59:00 -0500

    Adding_to_Dictionary

---
+++ Python/fundamentals/Dictionaries/adding_to_dictionary.py
@@ -0,0 +1,10 @@
+countries_so_far = {"Mexico": 1, "Morocco": 1}
+new_visits = ["Albania", "Mexico", "Togo", "Morocco"]
+    
+# To add Albania to the list
+countries_so_far["Albania"] = 1
+# To add 1 to the Mexico tally
+countries_so_far["Mexico"] += 1 # Changes Mexico tally to 2!
+# your code here to finish updating your travel log!
+countries_so_far["Togo"] = 1
+print(countries_so_far)
