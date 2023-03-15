ecf08f90159255e950583eafdf1fc57de4605bb0
Toe Head <torreyfeacher@gmail.com>
2023-03-14 21:23:41 -0400

    my_Television_class

---
+++ Python/fundamentals/my_Television_class.py
@@ -0,0 +1,13 @@
+
+
+
+class Television:
+    def __init__(self, screen_size, brand):
+        self.screen_size = ['screen_size']
+        self.brand = ['brand']
+
+my_tv1 = Television(60, 'QLED')
+print(my_tv1.screen_size)
+
+
+
