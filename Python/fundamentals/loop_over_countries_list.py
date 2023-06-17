a24c4d756bcca8bf8ffbefd6083e855ebb511135
Toe Head <torreyfeacher@gmail.com>
2023-06-17 16:01:31 -0400

    Made changes to loop_over_countries_list

--- Python/fundamentals/loop_over_countries_list.py
+++ Python/fundamentals/loop_over_countries_list.py
@@ -1,13 +1,41 @@
+"""
+
 countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
 
 # Challenge 1: Fix the range!
 for integer in range(0, len(countries)):
     print("Index:", integer)
+
     # Challenge 2: print the index here
     print("Country:", countries[integer])
+
     # Challenge 3: print the country here
  
 # Looping over values only...
 for i in range(0, len(countries)):
     print(i, "Country:", countries[i])
     # Challenge 4: print the country here
+
+"""
+
+countries2 = ["Canada", "Iceland", "Chile", "Egypt", "Greenland", "Belgium", "Germany"]
+
+# Challenge 1: Fix the range!
+for integer in range(0, len(countries2)):
+    print(f"Index: {integer}")
+
+    # Challenge 2: print the index here
+    print(f"Country: {integer}")
+
+    # Challenge 3: print the country here
+    print(f"Country: " + countries2[integer])
+ 
+# Looping over values only...
+for country in countries2:
+    print(f"Country: {country}")
+
+    # Challenge 4: print the country here
+for country in countries2:
+    print(f"{country}")
+
+
